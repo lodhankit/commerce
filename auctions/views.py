fc445e927b3493f5240c9a7a57bee3635b895c78
Ankit Lodh <ankit.intellegencesystem2019@gmail.com>
2023-05-10 11:33:43 +0530

    One can add comment for product but product comments shared among all products which is the problem that going to solve next

--- auctions/views.py
+++ auctions/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User,AuctionList
+from .models import User,AuctionList,Comments
 
 
 def index(request):
@@ -74,4 +74,20 @@ def register(request):
         return render(request, "auctions/register.html")
 
 def create(request):
-    return render(request, "auctions/createlist.html")
+    return render(request, "auctions/createlist.html")\
+    
+def details(request, product_id):
+    user = User.objects.get(username=request.user)
+    if(request.method == 'POST'):
+        comment = request.POST['comment']
+        comments = Comments.objects.create(username=user, comment=comment)
+        comments.save()
+    try:
+        comment_details = Comments.objects.get(username=user)
+    except:
+        comment_details = []
+    product_details = AuctionList.objects.get(id=product_id)
+    return render(request, "auctions/details.html",{
+        "details": product_details,
+        "comment_details":comment_details
+    })
